58887598b0a154770dd78e3f32c32982673de62a
Amandine Pouzin <amandinepouzin@hotmail.com>
2018-01-13 11:18:01 -0800

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'mad_lib/index'
+  get 'mad_lib', to: 'mad_lib#index'
 
-  get 'mad/Libs'
 
-  get 'mad/index'
+  match 'disneyworld', to: 'mad_lib#disneyworld', via: :post
+
+  get 'disneyworld', to: 'mad_lib#reroute', via: :get
+
+  match 'video_game', to: 'mad_lib#video_game', via: :post
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
